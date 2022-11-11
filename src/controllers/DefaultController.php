4e9a1d0aa4a63190c0d29dfcb198fcd7732ce83b
Gloria Kuliś <gloria.kulis@student.pk.edu.pl>
2022-11-11 17:11:25 +0100

    routing oraz struktura plików

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'AppController.php';
+
+class DefaultController extends AppController{
+
+    public function index(){
+        $this->render('login');
+    }
+
+    public function timer(){
+
+        $this->render('timer');
+    }
+
+    public function toDoList(){
+
+        $this->render('todolist');
+    }
+
+    public function statistics(){
+        
+        $this->render('statistics');
+    }
+
+    public function achivments(){
+        $this->render('achivments');
+    }
+
+
+}
