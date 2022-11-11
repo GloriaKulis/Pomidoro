c5aaaff123aedc47ea2b84f9ed7634ca05a14fba
Gloria Kuliś <gloria.kulis@student.pk.edu.pl>
2022-11-11 17:09:48 +0100

    routing oraz struktura plików

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 
-    echo "Hi!"; 
+require 'Routing.php';
 
+$path = trim($_SERVER['REQUEST_URI'],'/');
+$path = parse_url($path,PHP_URL_PATH);
+
+
+
+Routing::get('index','DefaultController');
+Routing::get('timer','DefaultController');
+Routing::get('toDoList','DefaultController');
+Routing::get('statistics','DefaultController');
+Routing::get('achivments','DefaultController');
+
+Routing::run($path);
